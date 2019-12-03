ea5eb6285e250f76fef380b957f7e812b7569e79
botond <belgianmaniax@gmail.com>
2019-12-03 11:32:58 +0100

    Deal cards into columns

--- static/js/dom.js
+++ static/js/dom.js
@@ -7,6 +7,7 @@ export const dom = {
     deck: {
         init: function () {
             const deck = this.generate();
+            this.display(deck);
         },
         generate: function () {
             let deck = [];
@@ -22,10 +23,26 @@ export const dom = {
         },
         createCard: function (suit, rank) {
             const card = document.createElement('DIV');
-            card.classList.add('card');
+            card.classList.add('card', 'unflipped');
             card.dataset.suit = suit;
             card.dataset.rank = rank.toString();
+            const cardHeader = document.createElement('DIV');
+            cardHeader.classList.add('card-header');
+            cardHeader.textContent = `${suit} ${rank}`;
+            card.appendChild(cardHeader);
             return card;
+        },
+        display: function (deck) {
+            let columns = document.querySelectorAll('.column');
+            columns = Array.from(columns);
+            for (let firstCol = 0; firstCol < 7; firstCol++) {
+                for (let col = firstCol; col < 7; col++) {
+                    const card = deck.pop();
+                    card.classList.toggle('unflipped', col !== firstCol);
+                    columns[col].appendChild(card);
+                }
+            }
+
         }
     }
 };
